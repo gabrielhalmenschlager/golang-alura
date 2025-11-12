40ee6b4ff8e1b14d8e1022d7e489d2ebb920320d
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-11 22:58:27 -0300

    Pacotes e Visibilidade

--- banco/main.go
+++ banco/main.go
@@ -1,56 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
-
-func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
-	if podeSacar {
-		c.saldo -= valorDoSaque
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"
-	}
-}
-
-func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
-	if valorDoDeposito > 0 {
-		c.saldo += valorDoDeposito
-		return "Deposito realizado com sucesso", c.saldo
-	} else {
-		return "Valor do deposito menor que zero", c.saldo
-	}
-}
-
-func (c *ContaCorrente) Transferir(valorDaTrasferencia float64, contaDestino *ContaCorrente) bool {
-	if valorDaTrasferencia < c.saldo && valorDaTrasferencia > 0 {
-		c.saldo -= valorDaTrasferencia
-		contaDestino.Depositar(valorDaTrasferencia)
-		return true
-	} else {
-		return false
-	}
-}
+	"github.com/gabrielhalmenschlager/curso-golang-alura/banco/contas"
+)
 
 func main() {
-	contaDoGabriel := ContaCorrente{
-		titular:       "Gabriel",
-		numeroAgencia: 589,
-		numeroConta:   123456,
-		saldo:         500,
+	contaDoGabriel := contas.ContaCorrente{
+		Titular:       "Gabriel",
+		NumeroAgencia: 589,
+		NumeroConta:   123456,
+		Saldo:         500,
 	}
 
-	contaDoPedro := ContaCorrente{
-		titular:       "Pedro",
-		numeroAgencia: 172,
-		numeroConta:   654321,
-		saldo:         700,
+	contaDoPedro := contas.ContaCorrente{
+		Titular:       "Pedro",
+		NumeroAgencia: 172,
+		NumeroConta:   654321,
+		Saldo:         700,
 	}
 
 	status := contaDoPedro.Transferir(200, &contaDoGabriel)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gabrielhalmenschlager/curso-golang-alura
+
+go 1.25.3
