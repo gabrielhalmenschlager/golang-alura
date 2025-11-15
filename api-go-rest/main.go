17233f3ce555cd3dc75b4552d2f4666fec5f237e
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-15 09:37:42 -0300

    Respondendo Json

--- api-go-rest/main.go
+++ api-go-rest/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 
+	"github.com/gabrielhalmenschlager/curso-golang-alura/api-go-rest/models"
 	"github.com/gabrielhalmenschlager/curso-golang-alura/api-go-rest/routes"
 )
 
 func main() {
+
+	models.Personalidades = []models.Personalidade{
+		{Nome: "Nome 1", Historia: "Historia 1"},
+		{Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
 	fmt.Println("iniciando o servidor Rest com Go")
 	routes.HandleRequest()
 }
