c502527898e2505f67e44847a5db9dbe1120b456
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-16 17:23:23 -0300

    Struct de Aluno

--- gin-api-rest/controllers/controller.go
+++ gin-api-rest/controllers/controller.go
@@ -1,12 +1,12 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gabrielhalmenschlager/curso-golang-alura/gin-api-rest/models"
+	"github.com/gin-gonic/gin"
+)
 
 func ExibeTodosAlunos(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"id":   1,
-		"nome": "Gabriel",
-	})
+	c.JSON(200, models.Alunos)
 }
 
 func Saudacao(c *gin.Context) {
--- gin-api-rest/main.go
+++ gin-api-rest/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/gabrielhalmenschlager/curso-golang-alura/gin-api-rest/routes"
+import (
+	"github.com/gabrielhalmenschlager/curso-golang-alura/gin-api-rest/models"
+	"github.com/gabrielhalmenschlager/curso-golang-alura/gin-api-rest/routes"
+)
 
 func main() {
+	models.Alunos = []models.Aluno{
+		{Nome: "Ana Silva", CPF: "123.456.789-00", RG: "12.345.678-9"},
+		{Nome: "Bruno Santos", CPF: "987.654.321-00", RG: "98.765.432-1"},
+		{Nome: "Carla Souza", CPF: "111.222.333-44", RG: "22.333.444-5"},
+		{Nome: "Diego Martins", CPF: "555.666.777-88", RG: "55.666.777-8"},
+		{Nome: "Eduarda Lima", CPF: "999.888.777-66", RG: "99.888.777-6"},
+	}
+
 	routes.HandleRequests()
 }
