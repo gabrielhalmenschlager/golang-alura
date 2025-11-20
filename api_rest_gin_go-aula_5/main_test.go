6b23fd6eb69a948c91feb1b57499aa8099861409
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-20 10:14:10 -0300

    Teste StatusCode

--- api_rest_gin_go-aula_5/main_test.go
+++ api_rest_gin_go-aula_5/main_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/gabrielhalmenschlager/curso-golang-alura/api_rest_gin_go-aula_5/controllers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,6 +14,17 @@ func SetupDasRotasDeTeste() *gin.Engine {
 	return rotas
 }
 
-func TestFalhador(t *testing.T) {
-	t.Fatalf("Teste falhou de propósito, não se preocupe!")
+// func TestFalhador(t *testing.T) {
+// 	t.Fatalf("Teste falhou de propósito, não se preocupe!")
+// }
+
+func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
+	r := SetupDasRotasDeTeste()
+	r.GET("/:nome", controllers.Saudacao)
+	req, _ := http.NewRequest("GET", "/gabe", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	if resposta.Code != http.StatusOK {
+		t.Fatalf("Status error: valor recebido foi %d e o esperado era %d", resposta.Code, http.StatusOK)
+	}
 }
