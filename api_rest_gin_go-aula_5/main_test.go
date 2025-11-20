ab6b34031448a2eeb20886be4b842a85031d46cb
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-20 18:55:56 -0300

    Testando a Listagem de um Recurso

--- api_rest_gin_go-aula_5/main_test.go
+++ api_rest_gin_go-aula_5/main_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/gabrielhalmenschlager/curso-golang-alura/api_rest_gin_go-aula_5/controllers"
+	"github.com/gabrielhalmenschlager/curso-golang-alura/api_rest_gin_go-aula_5/database"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
@@ -30,4 +31,14 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 	mockDaResposta := `{"API diz:":"E ai gabe, tudo beleza?"}`
 	respostaBody, _ := ioutil.ReadAll(resposta.Body)
 	assert.Equal(t, mockDaResposta, string(respostaBody))
+}
+
+func TestListandoTodosOsAlunosHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
 }
