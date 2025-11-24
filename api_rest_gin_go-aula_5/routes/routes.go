2f7d20eb03f56f29be2fdc84a119bd73a2406593
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-24 17:17:37 -0300

    Estilizando com CSS

--- api_rest_gin_go-aula_5/routes/routes.go
+++ api_rest_gin_go-aula_5/routes/routes.go
@@ -8,6 +8,7 @@ import (
 func HandleRequests() {
 	r := gin.Default()
 	r.LoadHTMLGlob("api_rest_gin_go-aula_5/templates/*")
+	r.Static("api_rest_gin_go-aula_5/assets", "./api_rest_gin_go-aula_5/assets")
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
 	r.GET("/:nome", controllers.Saudacao)
 	r.POST("/alunos", controllers.CriaNovoAluno)
