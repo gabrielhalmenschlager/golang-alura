890f31704a5439c4a227ddaa37438199f0e59374
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-24 15:32:28 -0300

    Criando um repositório próprio para a PokeAPI

--- pokeapi-go/models/favorito.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Favorito struct {
-	ID        uint    `json:"id" gorm:"primaryKey;autoIncrement"`
-	PokemonID uint    `json:"pokemon_id" gorm:"not null"`
-	Pokemon   Pokemon `json:"pokemon" gorm:"foreignKey:PokemonID"`
-}
--- pokeapi-go/models/pokemon.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Pokemon struct {
-	ID       uint   `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name     string `json:"name" gorm:"unique;not null"`
-	Type     string `json:"type" gorm:"not null"`
-	ImageURL string `json:"image_url"`
-}
