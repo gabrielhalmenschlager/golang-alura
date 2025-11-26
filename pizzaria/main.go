f5348b46855cebe312870034c41ba0d347d13a45
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-25 22:27:53 -0300

    Structs

--- pizzaria/main.go
+++ pizzaria/main.go
@@ -2,8 +2,15 @@ package main
 
 import "fmt"
 
+type Pizza struct {
+	ID    int
+	Nome  string
+	Preco float64
+}
+
 func main() {
 	nomePizzaria := "Pizzaria Go"
-	instagram, telefone := "@pizzaria_go", 5551999999999
+	instagram, telefone := "@pizzaria_go", 555199999999
+
 	fmt.Println(nomePizzaria, instagram, telefone)
 }
