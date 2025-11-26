c82dd211af2dcbbec1626dfa3e691ff43665efd0
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-25 22:17:43 -0300

    Variáveis e Conversões

--- pizzaria/main.go
+++ pizzaria/main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Pizzaria")
+	nomePizzaria := "Pizzaria Go"
+	instagram, telefone := "@pizzaria_go", 5551999999999
+	fmt.Println(nomePizzaria, instagram, telefone)
 }
