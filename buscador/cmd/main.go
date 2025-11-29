5048db8d9d38daafcbf28acf6ae21e1bf0e280b9
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-29 20:10:52 -0300

    Projeto Inicial

---
+++ buscador/cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/gabrielhalmenschlager/curso-golang-alura/buscador/internal/fetcher"
+)
+
+func main() {
+	start := time.Now()
+	price1 := fetcher.FetchPriceFromSite1()
+	price2 := fetcher.FetchPriceFromSite2()
+	price3 := fetcher.FetchPriceFromSite3()
+
+	fmt.Printf("R$ %.2f \n", price1)
+	fmt.Printf("R$ %.2f \n", price2)
+	fmt.Printf("R$ %.2f \n", price3)
+
+	fmt.Printf("\nTempo total: %s", time.Since(start))
+}
