f38c3ca71cf2e4f46edcbf4a1d811d36b987e752
“gabrielhalmenschlager” <gabrielh2007.scs@gmail.com>
2025-11-30 16:22:37 -0300

    Métodos e Funções

--- estoque-go/internal/cmd/main.go
+++ estoque-go/internal/cmd/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gabrielhalmenschlager/curso-golang-alura/estoque-go/internal/models"
+)
 
 func main() {
 	fmt.Println("Sistema de Estoque")
+
+	item1 := models.Item{
+		ID:       1,
+		Name:     "Primeiro Produto",
+		Quantity: 10,
+		Price:    19.99,
+	}
+	item2 := models.Item{
+		ID:       2,
+		Name:     "Segundo Produto",
+		Quantity: 5,
+		Price:    12.99,
+	}
+	item3 := models.Item{
+		ID:       3,
+		Name:     "Terceiro Produto",
+		Quantity: 15,
+		Price:    15.99,
+	}
+
+	fmt.Println(item1.Info())
+	fmt.Println(item2.Info())
+	fmt.Println(item3.Info())
 }
